226a8ecfa4dd8781a35e91c8a88bfaebe0236bda
DiegoLibonati <diego.libonati1998@gmail.com>
2022-11-12 00:37:00 -0300

    New readme

--- README.md
+++ README.md
@@ -39,6 +39,187 @@ I made a web page that allows the user to save notes. With the notes you can add
 
 ## Video
 
-
 https://user-images.githubusercontent.com/99032604/199378785-37ec1990-8cdf-47e6-b88d-44fb63787054.mp4
 
+## Documentation
+
+We obtain the elements of the dom that we are going to use to render the information:
+
+```
+const btnAddNote = document.querySelector(".header_container");
+const cardsContainer = document.querySelector(".section_container");
+const dataHeaderContainer = document.querySelector(".header_container h2");
+```
+
+By clicking on the `btnAddNote` button what we are going to do is to add a new note:
+
+```
+btnAddNote.addEventListener("click", () => {
+  let i = setIdCards();
+  newCard = true;
+
+  cardsContainer.innerHTML += `
+
+        <div class="card" id="card-${i}">
+            <div class="card_header">
+                <button type="button" class="btnEdit"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button type="button" class="btnDelete"><i class="fa-solid fa-trash"></i></button>
+            </div>
+
+            <div class="card_container">
+                <textarea rows="5" cols="2" disabled>Ingrese texto</textarea>
+            </div>
+        </div>
+
+    `;
+
+  actionAds();
+  editCard();
+  deleteCard();
+  newCard = false;
+});
+```
+
+The `editCard()` function will allow us to edit the information of a card, the card we click to edit:
+
+```
+function editCard() {
+  editedCard = true;
+  const btnsEdit = document.querySelectorAll(".btnEdit");
+  let cardsStorage = getLocalStorage();
+
+  btnsEdit.forEach(function (btnEdit) {
+    btnEdit.addEventListener("click", () => {
+      let cardId = btnEdit.parentElement.parentElement.id;
+      const cardFinalId = cardId.slice(5);
+
+      let cardTextArea =
+        btnEdit.parentElement.parentElement.children[1].children[0];
+      cardTextArea.disabled = false;
+
+      let cardContainerBtns = btnEdit.parentElement.parentElement.children[0];
+      cardContainerBtns.innerHTML += `<button type="button" class="btnFinishEdit">✓</button>`;
+
+      const btnFinishEdit = document.querySelector(".btnFinishEdit");
+
+      btnFinishEdit.addEventListener("click", () => {
+        for (let i = 0; i < cardsStorage.length; i++) {
+          console.log(cardFinalId, cardsStorage[i].id);
+          if (cardFinalId == cardsStorage[i].id) {
+            cardsStorage[i].text = cardTextArea.value;
+            localStorage.setItem("list", JSON.stringify(cardsStorage));
+          }
+        }
+
+        cardContainerBtns.children[2].remove();
+        cardTextArea.innerHTML = cardTextArea.value;
+        cardTextArea.disabled = true;
+
+        actionAds();
+        editCard();
+        deleteCard();
+        editedCard = false;
+      });
+    });
+  });
+}
+```
+
+The `deleteCard()` function will allow us to delete the card we clicked to delete:
+
+```
+function deleteCard() {
+  deletedCard = true;
+  const btnsDelete = document.querySelectorAll(".btnDelete");
+  let cardsStorage = getLocalStorage();
+
+  btnsDelete.forEach(function (btnDelete) {
+    btnDelete.addEventListener("click", () => {
+      btnDelete.parentElement.parentElement.remove();
+
+      const cardId = btnDelete.parentElement.parentElement.id.slice(5);
+
+      for (let i = 0; i < cardsStorage.length; i++) {
+        if (cardId == cardsStorage[i].id) {
+          const index = cardsStorage.indexOf(cardsStorage[i]);
+
+          cardsStorage.splice(index, 1);
+
+          localStorage.setItem("list", JSON.stringify(cardsStorage));
+        }
+      }
+      actionAds();
+      deletedCard = false;
+    });
+  });
+}
+```
+
+The `addCardToLocalStorage()` function will allow us to add a note to the LocalStorage:
+
+```
+function addCardToLocalStorage(id, text) {
+  let cardsStorage = getLocalStorage();
+
+  const card = { id: id, text: text };
+
+  cardsStorage.push(card);
+
+  localStorage.setItem("list", JSON.stringify(cardsStorage));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+```
+
+The `setIdCards()` function will allow us to add a unique id to each note we add in the LocalStorage, it will take as id the length of localstorage plus one:
+
+```
+function setIdCards() {
+  let cardsStorage = getLocalStorage();
+
+  if (cardsStorage == null) {
+    let contador = 1;
+    contador++;
+    addCardToLocalStorage(contador, "Ingrese texto");
+    return contador;
+  } else {
+    let contador = cardsStorage.length;
+    contador++;
+    addCardToLocalStorage(contador, "Ingrese texto");
+    return contador;
+  }
+}
+```
+
+The `loadCardsInLocalStorage()` function will allow us to load all the notes if they exist from LocalStorage:
+
+```
+function loadCardsInLocalStorage() {
+  let cardsStorage = getLocalStorage();
+
+  for (let i = 0; i < cardsStorage.length; i++) {
+    cardsContainer.innerHTML += `
+
+        <div class="card" id="card-${cardsStorage[i].id}">
+            <div class="card_header">
+                <button type="button" class="btnEdit"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button type="button" class="btnDelete"><i class="fa-solid fa-trash"></i></button>
+            </div>
+
+            <div class="card_container">
+                <textarea rows="5" cols="2" disabled>${cardsStorage[i].text}</textarea>
+            </div>
+        </div>
+
+    `;
+  }
+
+  actionAds();
+  editCard();
+  deleteCard();
+}
+```
--- codigo.js
+++ codigo.js
@@ -8,12 +8,11 @@ let deletedCard = false;
 
 loadCardsInLocalStorage();
 
-btnAddNote.addEventListener("click", ()=>{
+btnAddNote.addEventListener("click", () => {
+  let i = setIdCards();
+  newCard = true;
 
-    let i = setIdCards();
-    newCard = true;
-
-        cardsContainer.innerHTML += `
+  cardsContainer.innerHTML += `
     
         <div class="card" id="card-${i}">
             <div class="card_header">
@@ -27,135 +26,117 @@ btnAddNote.addEventListener("click", ()=>{
         </div>
     
     `;
-    
-    actionAds();
-    editCard();
-    deleteCard();
-    newCard = false;
+
+  actionAds();
+  editCard();
+  deleteCard();
+  newCard = false;
 });
 
+function editCard() {
+  editedCard = true;
+  const btnsEdit = document.querySelectorAll(".btnEdit");
+  let cardsStorage = getLocalStorage();
 
-function editCard(){
+  btnsEdit.forEach(function (btnEdit) {
+    btnEdit.addEventListener("click", () => {
+      let cardId = btnEdit.parentElement.parentElement.id;
+      const cardFinalId = cardId.slice(5);
 
-    editedCard = true;
-    const btnsEdit = document.querySelectorAll(".btnEdit");
-    let cardsStorage = getLocalStorage();
+      let cardTextArea =
+        btnEdit.parentElement.parentElement.children[1].children[0];
+      cardTextArea.disabled = false;
 
-    btnsEdit.forEach(function(btnEdit){
+      let cardContainerBtns = btnEdit.parentElement.parentElement.children[0];
+      cardContainerBtns.innerHTML += `<button type="button" class="btnFinishEdit">✓</button>`;
 
-        btnEdit.addEventListener("click", ()=>{
+      const btnFinishEdit = document.querySelector(".btnFinishEdit");
 
-            let cardId = btnEdit.parentElement.parentElement.id;
-            const cardFinalId = cardId.slice(5);
-
-            let cardTextArea = btnEdit.parentElement.parentElement.children[1].children[0];
-            cardTextArea.disabled = false;
-
-            let cardContainerBtns = btnEdit.parentElement.parentElement.children[0];
-            cardContainerBtns.innerHTML +=  `<button type="button" class="btnFinishEdit">✓</button>`;
-
-            const btnFinishEdit = document.querySelector(".btnFinishEdit");
-
-            btnFinishEdit.addEventListener("click", ()=>{
-
-                for (let i = 0; i < cardsStorage.length; i++){
-                    console.log(cardFinalId, cardsStorage[i].id)
-                    if (cardFinalId == cardsStorage[i].id){
-                        cardsStorage[i].text = cardTextArea.value;
-                        localStorage.setItem("list", JSON.stringify(cardsStorage));
-                    }
-
-                }
-
-                cardContainerBtns.children[2].remove();
-                cardTextArea.innerHTML = cardTextArea.value;
-                cardTextArea.disabled = true;
-
-                actionAds();
-                editCard();
-                deleteCard();
-                editedCard = false;
-            });
-        });
-
-    });
-
-}
-
-function deleteCard(){
-
-    deletedCard = true;
-    const btnsDelete = document.querySelectorAll(".btnDelete");
-    let cardsStorage = getLocalStorage();
-
-    btnsDelete.forEach(function(btnDelete){
-
-        btnDelete.addEventListener("click", ()=>{
-
-        btnDelete.parentElement.parentElement.remove();
-
-        const cardId = btnDelete.parentElement.parentElement.id.slice(5);
-
-        for (let i = 0; i < cardsStorage.length; i++){
-
-            if (cardId == cardsStorage[i].id){
-                const index = cardsStorage.indexOf(cardsStorage[i])
-                
-                cardsStorage.splice(index, 1);
-
-                localStorage.setItem("list", JSON.stringify(cardsStorage));
-
-            }
-
+      btnFinishEdit.addEventListener("click", () => {
+        for (let i = 0; i < cardsStorage.length; i++) {
+          console.log(cardFinalId, cardsStorage[i].id);
+          if (cardFinalId == cardsStorage[i].id) {
+            cardsStorage[i].text = cardTextArea.value;
+            localStorage.setItem("list", JSON.stringify(cardsStorage));
+          }
         }
+
+        cardContainerBtns.children[2].remove();
+        cardTextArea.innerHTML = cardTextArea.value;
+        cardTextArea.disabled = true;
+
         actionAds();
-        deletedCard = false;
-        });
-
+        editCard();
+        deleteCard();
+        editedCard = false;
+      });
     });
-
+  });
 }
 
-function addCardToLocalStorage(id, text){
+function deleteCard() {
+  deletedCard = true;
+  const btnsDelete = document.querySelectorAll(".btnDelete");
+  let cardsStorage = getLocalStorage();
 
-    let cardsStorage = getLocalStorage();
+  btnsDelete.forEach(function (btnDelete) {
+    btnDelete.addEventListener("click", () => {
+      btnDelete.parentElement.parentElement.remove();
 
-    const card = {id: id, text:  text};
+      const cardId = btnDelete.parentElement.parentElement.id.slice(5);
 
-    cardsStorage.push(card);
+      for (let i = 0; i < cardsStorage.length; i++) {
+        if (cardId == cardsStorage[i].id) {
+          const index = cardsStorage.indexOf(cardsStorage[i]);
 
-    localStorage.setItem("list", JSON.stringify(cardsStorage));
+          cardsStorage.splice(index, 1);
 
-}
-
-function getLocalStorage(){
-    return localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
-}
-
-function setIdCards(){
-
-    let cardsStorage = getLocalStorage();
-
-        if (cardsStorage == null){
-            let contador = 1;
-            contador++
-            addCardToLocalStorage(contador, "Ingrese texto");
-            return contador;
-        } else {
-            let contador = cardsStorage.length;
-            contador++
-            addCardToLocalStorage(contador, "Ingrese texto");
-            return contador;
+          localStorage.setItem("list", JSON.stringify(cardsStorage));
         }
-
+      }
+      actionAds();
+      deletedCard = false;
+    });
+  });
 }
 
-function loadCardsInLocalStorage(){
+function addCardToLocalStorage(id, text) {
+  let cardsStorage = getLocalStorage();
 
-    let cardsStorage = getLocalStorage();
+  const card = { id: id, text: text };
 
-    for (let i = 0; i < cardsStorage.length; i++){
-        cardsContainer.innerHTML += `
+  cardsStorage.push(card);
+
+  localStorage.setItem("list", JSON.stringify(cardsStorage));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
+function setIdCards() {
+  let cardsStorage = getLocalStorage();
+
+  if (cardsStorage == null) {
+    let contador = 1;
+    contador++;
+    addCardToLocalStorage(contador, "Ingrese texto");
+    return contador;
+  } else {
+    let contador = cardsStorage.length;
+    contador++;
+    addCardToLocalStorage(contador, "Ingrese texto");
+    return contador;
+  }
+}
+
+function loadCardsInLocalStorage() {
+  let cardsStorage = getLocalStorage();
+
+  for (let i = 0; i < cardsStorage.length; i++) {
+    cardsContainer.innerHTML += `
 
         <div class="card" id="card-${cardsStorage[i].id}">
             <div class="card_header">
@@ -169,59 +150,52 @@ function loadCardsInLocalStorage(){
         </div>
     
     `;
+  }
 
-    }
-
-    actionAds();
-    editCard();
-    deleteCard();
+  actionAds();
+  editCard();
+  deleteCard();
 }
 
+function actionAds() {
+  let cardsStorage = getLocalStorage();
 
-function actionAds(){
+  window.addEventListener("DOMContentLoaded", () => {
+    dataHeaderContainer.innerHTML = `${cardsStorage.length} notes were created ✅`;
 
-    let cardsStorage = getLocalStorage();
+    setTimeout(() => {
+      dataHeaderContainer.classList.add("show-data");
+    }, 500);
 
-    window.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(() => {
+      dataHeaderContainer.classList.remove("show-data");
+    }, 2000);
+  });
 
-        dataHeaderContainer.innerHTML = `${cardsStorage.length} notes were created ✅`;
+  if (newCard === true) {
+    dataHeaderContainer.innerHTML = `1 note has been successfully created ✅`;
+    dataHeaderContainer.classList.add("show-data");
 
-        setTimeout(()=>{
-            dataHeaderContainer.classList.add("show-data");
-        }, 500);
-    
-        setTimeout(()=>{
-            dataHeaderContainer.classList.remove("show-data");
-        }, 2000);
-    
-    });
+    setTimeout(() => {
+      dataHeaderContainer.classList.remove("show-data");
+    }, 1000);
+  }
 
+  if (editedCard === true) {
+    dataHeaderContainer.innerHTML = `1 note has been successfully edited ✅`;
+    dataHeaderContainer.classList.add("show-data");
 
-    if (newCard === true){
-        dataHeaderContainer.innerHTML = `1 note has been successfully created ✅`;
-        dataHeaderContainer.classList.add("show-data");
+    setTimeout(() => {
+      dataHeaderContainer.classList.remove("show-data");
+    }, 1000);
+  }
 
-        setTimeout(()=>{
-            dataHeaderContainer.classList.remove("show-data");
-        }, 1000);
-    }
+  if (deletedCard === true) {
+    dataHeaderContainer.innerHTML = `1 note has been successfully deleted ✅`;
+    dataHeaderContainer.classList.add("show-data");
 
-    if (editedCard === true){
-        dataHeaderContainer.innerHTML = `1 note has been successfully edited ✅`;
-        dataHeaderContainer.classList.add("show-data");
-
-        setTimeout(()=>{
-            dataHeaderContainer.classList.remove("show-data");
-        }, 1000);
-    }
-
-    if (deletedCard === true){
-        dataHeaderContainer.innerHTML = `1 note has been successfully deleted ✅`;
-        dataHeaderContainer.classList.add("show-data");
-
-        setTimeout(()=>{
-            dataHeaderContainer.classList.remove("show-data");
-        }, 1000);
-    }
-
+    setTimeout(() => {
+      dataHeaderContainer.classList.remove("show-data");
+    }, 1000);
+  }
 }
